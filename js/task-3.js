ab4fd0aadb200d862af1e655efb69d2a0e3c0a59
DenysStepanenko <denys.workspace@gmail.com>
2025-02-17 23:40:24 -0700

    Remove launch configuration for Microsoft Edge and enhance getElementWidth function with clearer calculations and additional console log examples

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,11 @@ function getElementWidth(content, padding, border) {
     const contentWidth = Number.parseFloat(content);
     const paddingWidth = Number.parseFloat(padding);
     const borderWidth = Number.parseFloat(border);
-
-    return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
+    const totalWidthPadding = paddingWidth * 2;
+    const totalWidthBorder = borderWidth * 2;
+    return contentWidth + totalWidthPadding + totalWidthBorder;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
 
