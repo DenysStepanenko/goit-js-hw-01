01a6e5c74a2b4becb68b74f85baf40bd0b860f43
Denys Stepanenko <denys.workspace@gmail.com>
2025-02-26 01:42:43 -0700

    Update index.html title and script tags; add 'use strict' to JavaScript files

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+'use strict';
 function getElementWidth(content, padding, border) {
     const contentWidth = Number.parseFloat(content.replace('px', ''));
     const paddingWidth = Number.parseFloat(padding.replace('px', ''));
